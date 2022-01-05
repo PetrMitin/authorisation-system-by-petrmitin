263d49d4f24e4a810119a8be10f9f6c73f85299c
Petr Mitin <petr09mitin@mail.ru>
2022-01-05 15:26:27 +0300

    Git version

--- server/server.js
+++ server/server.js
@@ -3,12 +3,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 const router = require('./router')
 
 mongoose.connect(process.env.MONGODB_URI)
 
 const app = express()
 const PORT = process.env.PORT || 4000
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '..', 'build')))
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
+    })
+  }  
 
 app.use(express.json())
 app.use(cors({
